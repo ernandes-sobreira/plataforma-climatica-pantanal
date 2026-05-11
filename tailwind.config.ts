23b4a851688aa5ac843f39114a9f78c9090c592d
ernandes-sobreira <156730719+ernandes-sobreira@users.noreply.github.com>
2026-05-11 17:43:05 -0400

    Add tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./client/src/**/*.{ts,tsx}', './client/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        pantanal: {
+          50: '#f0fdf4',
+          100: '#dcfce7',
+          500: '#22c55e',
+          600: '#16a34a',
+          700: '#15803d',
+          800: '#166534',
+          900: '#14532d',
+        }
+      }
+    }
+  },
+  plugins: [],
+} satisfies Config
